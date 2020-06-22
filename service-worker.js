f3c8f2cb1af9f1b33b3e91b1f0cde201cb4d668f
bran4u <s.oselskiy@gmail.com>
2020-06-22 19:03:21 +0300

    Updates

--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "./precache-manifest.0be2a4d51bf87742479c1b1baf41cd4d.js"
+  "./precache-manifest.19504ac65a6d4e6adf2f53701979b628.js"
 );
 
 self.addEventListener('message', (event) => {
